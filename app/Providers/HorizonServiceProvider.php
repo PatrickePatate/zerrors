e020892e0ad4e83b716b736d38f582b17c6be3f3
Lucien <lucienpuget1@gmail.com>
2026-09-10 21:02:26 +0200

    Fix horizon access gate

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -28,9 +28,7 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     protected function gate(): void
     {
         Gate::define('viewHorizon', function ($user = null) {
-            return in_array(optional($user)->email, [
-                //
-            ]);
+            return str($user->email)->endsWith(['@example.com', '@example.org', 'you@example.com']);
         });
     }
 }
